06f2997040879ece5bbeea1cbe11dc5982c62d1d
gabrielsavian <gabrielsvnzrd@gmail.com>
2022-04-05 15:34:44 -0300

    first-commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+const App = () => {
+  return (
+      <div className="App">
+        <h1>Hello from AlleSys</h1>
+      </div>
+  )
+}
+
+export default App;
