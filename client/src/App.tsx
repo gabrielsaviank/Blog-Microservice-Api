ed9006403b354c9b1b996995a54470a57ac4d063
gabrielsavian <gabrielsvnzrd@gmail.com>
2022-04-11 08:17:53 -0300

    chore: fixed cors and types

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,7 @@ const App = () => {
 
           <PostCreate/>
       </div>
-  )
-}
+  );
+};
 
 export default App;
--- comments-service/index.ts
+++ comments-service/index.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response} from "express";
 import bodyParser from "body-parser";
 import { randomBytes } from "crypto";
+import cors from "cors";
 
 const app = express();
-
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId: any = {
 
